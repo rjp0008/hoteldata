a9b3e7c634a42d4f68456d3dd828bc06c2d42a4b
Robert Perez <robertjasonperez@gmail.com>
2016-10-27 00:34:38 -0500

    Testing interaction with ihg website to scrape data

--- testDriver.py
+++ testDriver.py
@@ -1,27 +1,71 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-import time
+import requests, bs4, sys
+countryList = 'https://www.ihg.com/destinations/us/en/explore'
 
-hyatt_category_page = 'https://www.hyatt.com/gp/en/awards/hyatt_category_display.jsp?_DARGS=/gp/en/awards/hyatt_category_display.jsp'
-browser = webdriver.Firefox()
-browser.get(hyatt_category_page)
-select = Select(browser.find_element_by_tag_name('select'))
-select.select_by_value('2')
+class HyattHotel:
+    def __init__(self):
+        self.url = ""
+        self.address = ""
+        self.name = ""
 
-time.sleep(2)
-links = browser.find_elements_by_tag_name('a')
-hotel_links = []
-for link in links:
-    url = link.get_attribute('href')
-    if(str(url).__contains__('/hotel/')):
-        hotel_links.append(url)
-        print(url)
-addresses = []
-for link in hotel_links:
-    browser.get(link)
-    elements = browser.find_elements_by_class_name('address')
-    address = ""
-    for item in elements:
-        address = address + item.text + " "
-    addresses.append(address)
-    print(address)
+    def __str__(self):
+        return self.url + " " + self.address
+
+hotels = []
+
+def getHotelInfo(link):
+    hotel = HyattHotel()
+    hotel.url = link
+    res = requests.get(link)
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.text)
+    address = soup.select('span[itemprop="address"]')
+    hotel.address = address[0].getText()
+    hotel.name = soup.select('h2[class="hi-hd-quick-info-address-heading"]')[0].getText()
+    hotels.append(hotel)
+    with open('ihg.txt', 'w') as f:
+        for hotel in hotels:
+            f.write(hotel.name + "\n")
+            f.write("\t" + hotel.url + "\n")
+            try:
+                f.write("\t" + str(hotel.address).encode(sys.stdout.encoding,errors='replace').decode('utf-8') + "\n")
+            except:
+                pass
+
+def possibleValidLink(url):
+    if 'airport-hotels' in url:
+        return False
+    if 'waterpark-holidome-hotels' in url:
+        return False
+    if 'beach-hotels' in url:
+        return False
+    if 'pet-friendly-hotels' in url:
+        return False
+    return True
+
+def drillDown(link,ignoreList):
+    res = requests.get(link)
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.text)
+    links = soup.select('a')
+    for item in links:
+        newLink = item.attrs['href']
+        if 'explore' in newLink:
+            continue
+        if not possibleValidLink(newLink):
+            break
+        if '/destinations/us/en/' in newLink and newLink not in ignoreList:
+            ignoreList.append(newLink)
+            drillDown(newLink,ignoreList)
+        if '/hoteldetail' in newLink:
+            getHotelInfo(newLink)
+
+ignoreLinks = []
+res = requests.get(countryList)
+res.raise_for_status()
+soup = bs4.BeautifulSoup(res.text)
+links = soup.select('li > a')
+for item in links:
+    link = item.attrs['href']
+    if '/destinations/us/en/' in link:
+        ignoreLinks.append(link)
+        drillDown(link,ignoreLinks)
